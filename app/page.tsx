b0af8830f74aaa9a17ef467b7cfa126e3e7d1c9d
Duru Favour <favourduru24@gmail.com>
2026-03-11 02:43:47 -0700

    project and shadcn-ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
      <div>
       <h1>Hello World</h1>
+      <ThemeToggle/>
      </div>
   );
 }
---
+++ dummy.md
@@ -0,0 +1,7 @@
+npm install -g pnpm@latest-10
+
+pnpm run dev
+
+pnpm dlx shadcn@latest init
+
+pnpm dlx shadcn@latest add tooltip toggle sonner skeleton popover label input form dropdown-menu dialog collapsible card button avatar
